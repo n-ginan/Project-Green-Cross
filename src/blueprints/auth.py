47ab21b4717d42490ab6d458ebaadb35597ab347
papsykle <toramgarcia@gmail.com>
2025-01-11 16:46:36 +0800

    add(admin-dashls)

--- src/blueprints/auth.py
+++ src/blueprints/auth.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
-from flask_login import UserMixin
+from flask import Blueprint, render_template
+from flask_login import UserMixin, login_required
 
 auth_bp = Blueprint(
     "auth",
@@ -12,3 +12,9 @@ class User(UserMixin):
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
+
+@auth_bp.route("/admin-dashboard", methods=["GET"])
+@login_required
+def admin_dashboard():
+    # Should return staff and doctor data to show on the front-end side
+    return render_template("admin-dashboard.html")
